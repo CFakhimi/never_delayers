b264c46508cf6ecfe4bbc1ce0f121d8277123780
Alexander Schumann <aschuma4@db8.cse.nd.edu>
2024-12-09 08:39:00 -0500

    Form choice persists over refresh

--- static/js/home.js
+++ static/js/home.js
@@ -2,40 +2,37 @@
 //     showPredictionForm(); // Automatically display the first form (the prediction form) on page load
 // });
 
-function selectOption(event, element) {
-  event.preventDefault();
+function showForm(formType) {
+    // Hide all forms
+    const forms = document.querySelectorAll('.dataInput');
+    forms.forEach(form => form.classList.remove('active'));
 
-  // Get the form type from the 'form-type' attribute of the clicked element
-  const formType = element.getAttribute('form-type');
+    // Show the selected form
+    const selectedForm = document.getElementById(formType);
+    if (selectedForm) {
+        selectedForm.classList.add('active');
+    }
 
-  // Hide all forms
-  const forms = document.querySelectorAll('.dataInput');
-  forms.forEach(form => form.classList.remove('active'));
-
-  // Show the selected form
-  const selectedForm = document.getElementById(formType);
-  if (selectedForm) {
-    selectedForm.classList.add('active');
-  }
-
-  // Update the button label to match the clicked option's inner text
-  const dropdownButton = document.getElementById('dropdownMainButton');
-  dropdownButton.innerHTML = `${element.innerText} <span class="arrow">&#9662;</span>`;
+    // Update the button label to match the clicked option's inner text
+    const dropdownButton = document.getElementById('dropdownMainButton');
+    const element = document.getElementById(formType + 'Button');
+    dropdownButton.innerHTML = `${element.innerText} <span class="arrow">&#9662;</span>`;
 }
 
-// function selectOption(event, formType) {
-//   event.preventDefault();
+function selectOption(element) {
+    // Get the form type from the 'form-type' attribute of the clicked element
+    const formType = element.getAttribute('form-type');
 
-//   // Hide all forms
-//   const forms = document.querySelectorAll('.dataInput');
-//   forms.forEach(form => form.classList.remove('active'));
+    localStorage.setItem('selectedForm', formType);
 
-//   // Show the selected form
-//   const selectedForm = document.getElementById(formType);
-//   if (selectedForm) {
-//     selectedForm.classList.add('active');
-//   }
-// }
+    // Hide all forms
+    showForm(formType);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedValue = localStorage.getItem('selectedForm') || 'predict';
+    showForm(savedValue);
+})
 
 function showDropdown() {
     document.getElementById('formSelect').style.display = "block";
@@ -45,47 +42,19 @@ function hideDropdown() {
     document.getElementById('formSelect').style.display = "none";
 }
 
-// function showPredictionForm() {
-//     document.getElementById('delayPrediction').style.display = "block";
-//     document.getElementById('delayUpload').style.display = "none";
-//     document.getElementById('delayEdit').style.display = "none";
-//     document.getElementById('delayDelete').style.display = "none";
-// }
-
-// function showUploadForm() {
-//     document.getElementById('delayPrediction').style.display = "none";
-//     document.getElementById('delayUpload').style.display = "block";
-//     document.getElementById('delayEdit').style.display = "none";
-//     document.getElementById('delayDelete').style.display = "none";
-// }
-
-// function showEditForm() {
-//     document.getElementById('delayPrediction').style.display = "none";
-//     document.getElementById('delayUpload').style.display = "none";
-//     document.getElementById('delayEdit').style.display = "block";
-//     document.getElementById('delayDelete').style.display = "none";
-// }
-
-// function showDeleteForm() {
-//     document.getElementById('delayPrediction').style.display = "none";
-//     document.getElementById('delayUpload').style.display = "none";
-//     document.getElementById('delayEdit').style.display = "none";
-//     document.getElementById('delayDelete').style.display = "block";
-// }
-
 function getInputType() {
     const inputContainer = document.getElementById("editInputContainer");
     inputContainer.innerHTML = "";
     const inputType = document.getElementById("attributeSelection").value;
     switch (inputType) {
         case "Airline":
-            inputContainer.innerHTML = "<input type='text' maxlength='255' name='new_value' placeholder='Enter new airline' required>";
+            inputContainer.innerHTML = "<input list='airlines' name='new_value' required placeholder='Airline (e.g. United)'>";
             break;
         case "Origin":
-            inputContainer.innerHTML = "<input type='text' maxlength='3' name='new_value' placeholder='Enter new origin airport' required>";
+            inputContainer.innerHTML = "<input type='text' maxlength='3' name='new_value' required placeholder='Origin Airport (e.g. SFO) '>";
             break;
         case "Destination":
-            inputContainer.innerHTML = "<input type='text' maxlength='3' name='new_value' placeholder='Enter new destination airport' required>";
+            inputContainer.innerHTML = "<input type='text' maxlength='3' name='new_value' required placeholder='Destination Airport (e.g. LAX)'>";
             break;
         case "DepartureDate":
             inputContainer.innerHTML = "<input type='date' name='new_value' required>";
